ffa3db36996707e3cd72446e06045c40088b2410
Ingo Fahrentholz <ingo.fahrentholz@diva-e.com>
2022-04-01 12:37:45 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 const { resolve } = require('path')
 import { defineConfig } from 'vite'
+import { baseUrl } from "rollup-plugin-base-url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    assetsDir: "resources",
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-          element1: resolve(__dirname, 'src/my-element.ts'),
-          element2: resolve(__dirname, 'src/my-second-element.ts')
-      }
-    }
-  }
-})
+        element1: resolve(__dirname, "src/my-element.ts"),
+        element2: resolve(__dirname, "src/my-second-element.ts"),
+      },
+    },
+  },
+});
